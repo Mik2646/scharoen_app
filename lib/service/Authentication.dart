549f33a04d58828edcebc10c78b40cb342087880
acartdev <tnpkng@gmail.com>
2024-01-12 15:49:26 +0700

    จบบบบบ

--- lib/service/Authentication.dart
+++ lib/service/Authentication.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 // class AuthenticationService {
 //   FirebaseAuth _auth = FirebaseAuth.instance;
 //   User? get currentUser => auth.currentUser;
@@ -25,7 +24,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 //     return auth.authStateChanges();
 //   }
 
-
 //   Future<void>login()async{
 //     try{
 //       await auth.signInWithEmailAndPassword(email: 'email', password: 'password');
@@ -45,35 +43,32 @@ import 'package:firebase_auth/firebase_auth.dart';
 //   }
 // }
 
-class AuthenticationService{
-   FirebaseAuth _auth = FirebaseAuth.instance;
+class AuthenticationService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
- get currentUser => _auth.currentUser;
-    Future<User?> regiterWithEmailAndPassword(String email, String password) async{
-    try{
-     UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-     return credential.user;
-    }
-    catch(e){
+  get currentUser => _auth.currentUser;
+  Future<User?> regiterWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return credential.user;
+    } catch (e) {
       print("error");
     }
-    return null;
-     
-}
- Stream<User?>authStateChanges(){
+  }
+
+  Stream<User?> authStateChanges() {
     return _auth.authStateChanges();
   }
 
-   Future<User?> LoginWithEmailAndPassword(String email, String password) async{
-    try{
-     UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
-     return credential.user;
-    }
-    catch(e){
+  Future<User?> LoginWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return credential.user;
+    } catch (e) {
       print("error");
     }
-    return null;
-     
+  }
 }
-}
-
